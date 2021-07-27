b537c7dabcd5938faedfbf6cc466ebc189775d70
j2frise <jarce.boukoro@korian.fr>
2021-07-27 09:46:55 +0200

    remove appIdKey

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const config = {
-  appId: '662618426239-u770377l0a41o3390e07fhca23clqbh9.apps.googleusercontent.com',
-  secretKey: 'WrFGLvztrz4vbDp7POiw5574',
+  appId: null,
+  secretKey: null,
   scope: ["profile", "email"]
 }
 
